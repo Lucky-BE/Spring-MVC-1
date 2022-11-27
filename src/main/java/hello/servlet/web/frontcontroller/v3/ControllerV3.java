f719bdfbaa8b3e55257c3e28d99441c70153cdf7
Lucky-BE <byungeun96@gmail.com>
2022-11-27 14:47:10 +0900

    스프링 MVC1편 - 섹션 4
    -v4 까지

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -0,0 +1,11 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+
+import java.util.Map;
+
+public interface ControllerV3 {
+
+    ModelView process(Map<String, String> paramMap); //직접 만든 프레임워크에 종속적인 것 서블릿에 종속적이지 않다
+
+}
